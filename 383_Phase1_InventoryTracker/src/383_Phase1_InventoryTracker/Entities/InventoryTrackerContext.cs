9a1b2db6666257b5c0b68130694e2c679064f823
DESKTOP-F51O0BK\Aayush Dahal <aayush.dahal@selu.edu>
2017-02-02 19:39:16 -0600

    Initial Project setup, Both code first databases set up.

---
+++ 383_Phase1_InventoryTracker/src/383_Phase1_InventoryTracker/Entities/InventoryTrackerContext.cs
@@ -0,0 +1,28 @@
+﻿using _383_Phase1_InventoryTracker.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _383_Phase1_InventoryTracker.Entities
+{
+    public class InventoryTrackerContext : DbContext
+    {
+
+        public InventoryTrackerContext(DbContextOptions<InventoryTrackerContext> options) : base(options)
+        {
+
+        }
+     
+        public DbSet<User> Users { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           // optionsBuilder.UseSqlite("Filename=Inventory.db");
+        }
+
+    }
+}
+
